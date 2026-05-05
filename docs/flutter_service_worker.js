157a5250c2857c0072359fe3afb297ddc6022631
Your Name <you@example.com>
2026-05-05 07:49:37 +0700

    Update GitHub Pages deployment

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -42,7 +42,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ad0cd637e6102dc27752b6b49996cbdd
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "073267cf514a7e342150faf40e804eb0",
+"flutter_bootstrap.js": "12df0441eb12aed43ac9bffb7582b9ec",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
